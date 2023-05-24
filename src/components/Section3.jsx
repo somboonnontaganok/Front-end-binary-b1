9a4c5d52baf5365cfd4c9a2ea1df8513b36e163e
Shessire <wisuwatpond01@gmail.com>
2023-05-24 13:09:18 +0700

    create carousel

--- src/components/Section3.jsx
+++ src/components/Section3.jsx
@@ -4,49 +4,78 @@ import wt from '../assets/img/Weight training-card.jpg';
 import st from '../assets/img/Strangth training-card.jpg';
 import pt from '../assets/img/Pilates-card.jpg';
 import yt from '../assets/img/Yoga-card.jpg';
+import Carousel from 'react-bootstrap/Carousel';
 
 const Section3 = () => {
   return (
     <>
-        <div className="3-section my-5">
-        <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
-            <div className="carousel-inner">
-              <div className="carousel-item active" data-bs-interval="1000">
-                <img src={hiit} className="d-block w-50" alt="..."/>
-                <h5>Title</h5>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam ullam, ducimus dolor fugit aliquam unde quasi molestiae vero placeat ut libero aut adipisci nihil velit maxime, tempore magni sed odit.</p>
-            </div>
-              <div className="carousel-item" data-bs-interval="2000">
-                <img src={wt} className="d-block w-50" alt="..."/>
-                <h5>Title</h5>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam ullam, ducimus dolor fugit aliquam unde quasi molestiae vero placeat ut libero aut adipisci nihil velit maxime, tempore magni sed odit.</p>
-            </div>
-              <div className="carousel-item" data-bs-interval="2000">
-                <img src={st} className="d-block w-50" alt="..."/>
-                <h5>Title</h5>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam ullam, ducimus dolor fugit aliquam unde quasi molestiae vero placeat ut libero aut adipisci nihil velit maxime, tempore magni sed odit.</p>
-            </div>
-              <div className="carousel-item" data-bs-interval="2000">
-                <img src={pt} className="d-block w-50" alt="..."/>
-                <h5>Title</h5>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam ullam, ducimus dolor fugit aliquam unde quasi molestiae vero placeat ut libero aut adipisci nihil velit maxime, tempore magni sed odit.</p>
-            </div>
-              <div className="carousel-item">
-                <img src={yt} className="d-block w-50" alt="..."/>
-                <h5>Title</h5>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam ullam, ducimus dolor fugit aliquam unde quasi molestiae vero placeat ut libero aut adipisci nihil velit maxime, tempore magni sed odit.</p>
-            </div>
-            </div>
-            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span className="visually-hidden">Previous</span>
-            </button>
-            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-              <span className="carousel-control-next-icon" aria-hidden="true"></span>
-              <span className="visually-hidden">Next</span>
-            </button>
-          </div>
-    </div>
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={hiit}
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={wt}
+              alt="Second slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={st}
+              alt="Third slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={pt}
+              alt="Fourth slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={yt}
+              alt="Fifth slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
     </>
   )
 }
